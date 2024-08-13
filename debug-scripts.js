d8f58d6ed5c8f68cb1d1d76bdab2468aa38081aa
Miha Hozjan <miha.hozjan@inova.si>
2024-08-13 12:45:17 +0200

    Added a JS function

---
+++ debug-scripts.js
@@ -0,0 +1,7 @@
+(function() {
+    // Display an alert to confirm that the script is running
+    alert("JavaScript injection test successful!");
+
+    // Log a message to the console for further confirmation
+    console.log("JavaScript injection test successful!");
+})();
