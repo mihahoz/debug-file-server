3cfda905481fdd76f1f56cd567ad48012b8cd430
Miha Hozjan <34736840+mihahoz@users.noreply.github.com>
2024-09-24 13:27:31 +0200

    Update inject-property.js

--- inject-property.js
+++ inject-property.js
@@ -1,3 +1,3 @@
+console.log('remote script will set property');
 var tws_injected = "Inova IT, d.o.o.";
-alert('Script alert!');
-console.log('injected');
+console.log('remote script did set property');
